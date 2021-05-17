de58875cb18cfbebe59d05abebf602a27b1368b0
kaz <kaz@192.168.1.7>
2021-05-17 16:00:56 +0900

    てすと2

--- JavaTest1/src/test/JavaTest1.java
+++ JavaTest1/src/test/JavaTest1.java
@@ -4,8 +4,8 @@ public class JavaTest1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("test"); 
-		System.out.println("test"); 
+		System.out.println("test1"); 
+		System.out.println("test2"); 
 	}
 
 }
